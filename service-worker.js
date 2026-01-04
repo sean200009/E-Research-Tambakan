a12110083609152defd7686989959fd17594e425
sean200009 <haoseanemerson@gmail.com>
2026-01-04 21:26:10 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,24 @@
-const cacheName = "e-research-cache-v1";
-const filesToCache = [
-  "/",
-  "/index.html",
-  "/style.css.txt",
-  "/script.js",
-  "/papers.json",
-  "/manifest.json"
+const CACHE_NAME = 'studies-repo-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/admin.html',
+    '/manifest.json',
+    '/papers.json',
+    '/script.js',
+    '/style.css.txt'
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
