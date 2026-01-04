383ef2ed7bdf566796d40c75d217fd4aa0760940
sean200009 <haoseanemerson@gmail.com>
2026-01-05 00:31:42 +0800

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='study-repo-cache-v2';
+const urlsToCache=['index.html','admin.html','style.css.txt','script.js','papers.json','manifest.json'];
+
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
